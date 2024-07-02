dcdca2b3ff6e1f9a6a0dd0860b7dd9d01e111490
y-emam <YasserEmam2002@gmail.com>
2024-07-02 20:06:59 +0300

    removed user_id until we make login and signup

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # router = APIRouter(prefix="/chatbot")
 
 class UserData(BaseModel):
-    user_id: str
+    # user_id: str
     user_question: str
 
 @app.get('/chatbot')
@@ -34,9 +34,10 @@ def home():
 async def chatbot_general(user_data: UserData):
     chatbotModel = data['chatbotModel']
 
-    user_id = user_data.user_id
     user_question = user_data.user_question 
-
+    # user_id = user_data.user_id
+    user_id = "665f995565bb190409468564"
+    
     answer = chatbotAskQuestion(user_question, user_id, chatbotModel)
     
     return answer
@@ -44,7 +45,7 @@ async def chatbot_general(user_data: UserData):
 class DiseaseData(BaseModel):
     plantName: str
     diseaseName: str
-    user_id: str
+    # user_id: str
 
 @app.post('/chatbot/get-cure')
 async def chatbot_general(disease_date: DiseaseData):
@@ -52,7 +53,8 @@ async def chatbot_general(disease_date: DiseaseData):
 
     plant_name = disease_date.plantName
     disease_name = disease_date.diseaseName 
-    user_id = disease_date.user_id
+    # user_id = disease_date.user_id
+    user_id = "665f995565bb190409468564"
 
     # clear chat history before getting the cure
     chatbotModel.clear_history(user_id)
@@ -61,13 +63,14 @@ async def chatbot_general(disease_date: DiseaseData):
     
     return answer
 
-class ClearData(BaseModel):
-    user_id: str
+# class ClearData(BaseModel):
+    # user_id: str
 
 @app.post('/chatbot/clear-history')
-async def clear_history_route(clearData: ClearData):
+async def clear_history_route():
     chatbotModel = data['chatbotModel']
 
-    user_id = clearData.user_id
+    # user_id = clearData.user_id
+    user_id = "665f995565bb190409468564"
 
     return chatbotModel.clear_history(user_id)
